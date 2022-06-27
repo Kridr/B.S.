e1f7676900acacd6e6e2bf45d226356dfd4e5e33
Nikita <54816487+Kridr@users.noreply.github.com>
2022-06-27 08:42:46 +0300

    Added data preparation for SentencePiece

--- data_extractor.py
+++ data_extractor.py
@@ -4,39 +4,44 @@ from collections import defaultdict, Counter
 
 xml_file = 'annot.opcorpora.xml'
 corp_cased = 'corp_cased.csv'
+corp_sent = 'sent.txt'
 
 named_entities = set(['Name', 'Surn', 'Patr', 'Geox'])
 
 tree = ET.ElementTree(file=xml_file)
 root = tree.getroot()
 
-with open(corp_cased, mode='w', encoding='utf-8') as cc:
-    for text in root.iter('text'):
-        for paragraphs in text.iter('paragraphs'):
-            for paragraph in paragraphs.iter('paragraph'):
-                for sentence in paragraph.iter('sentence'):
-                    for tokens in sentence.iter('tokens'):
-                        word_list = []
-                        pos_ner_list = []
-                        for token in tokens.iter('token'):
-                            l = token.find('tfr').find('v').find('l')
-                            
-                            if l.attrib['id'] != '0' or l.find('g').attrib['v'] == 'UNKN':
-                                pos_ner = ''
-                                for g in l.iter('g'):
-                                    pn = g.attrib['v']
-                                    if pn in named_entities:
-                                        pos_ner = pn
-                                        break
-                                if not pos_ner:
-                                    pos_ner = l.find('g').attrib['v']
+with open(corp_sent, mode='w', encoding='utf-8') as cs:
+    with open(corp_cased, mode='w', encoding='utf-8') as cc:
+        for text in root.iter('text'):
+            for paragraphs in text.iter('paragraphs'):
+                for paragraph in paragraphs.iter('paragraph'):
+                    for sentence in paragraph.iter('sentence'):
+                        for tokens in sentence.iter('tokens'):
+                            word_list = []
+                            pos_ner_list = []
+                            for token in tokens.iter('token'):
+                                l = token.find('tfr').find('v').find('l')
+                                
+                                if l.attrib['id'] != '0' or l.find('g').attrib['v'] == 'UNKN':
+                                    pos_ner = ''
+                                    for g in l.iter('g'):
+                                        pn = g.attrib['v']
+                                        if pn in named_entities:
+                                            pos_ner = pn
+                                            break
+                                    if not pos_ner:
+                                        pos_ner = l.find('g').attrib['v']
 
 
 
-                                word_list.append(token.attrib['text'])
-                                pos_ner_list.append(pos_ner)
+                                    word_list.append(token.attrib['text'])
+                                    pos_ner_list.append(pos_ner)
 
-                        cc.write(' '.join(word_list))
-                        cc.write(',')
-                        cc.write(' '.join(pos_ner_list))
-                        cc.write('\n')
+                            cc.write(' '.join(word_list))
+                            cc.write(',')
+                            cc.write(' '.join(pos_ner_list))
+                            cc.write('\n')
+
+                            cs.write(' '.join(word_list))
+                            cs.write('\n')
